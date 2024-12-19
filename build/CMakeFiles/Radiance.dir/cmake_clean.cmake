a4d3661989389584b36ca67da0c097d55ad9af58
Amaan Khan <mak26@sfu.ca>
2024-12-18 22:06:19 -0800

    Fixed directory tree

---
+++ build/CMakeFiles/Radiance.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Radiance.dir/src/BVH.cpp.o"
+  "CMakeFiles/Radiance.dir/src/BVH.cpp.o.d"
+  "CMakeFiles/Radiance.dir/src/Camera.cpp.o"
+  "CMakeFiles/Radiance.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/Radiance.dir/src/Ray.cpp.o"
+  "CMakeFiles/Radiance.dir/src/Ray.cpp.o.d"
+  "CMakeFiles/Radiance.dir/src/Renderer.cpp.o"
+  "CMakeFiles/Radiance.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/Radiance.dir/src/Scene.cpp.o"
+  "CMakeFiles/Radiance.dir/src/Scene.cpp.o.d"
+  "CMakeFiles/Radiance.dir/src/Sphere.cpp.o"
+  "CMakeFiles/Radiance.dir/src/Sphere.cpp.o.d"
+  "CMakeFiles/Radiance.dir/src/main.cpp.o"
+  "CMakeFiles/Radiance.dir/src/main.cpp.o.d"
+  "bin/Radiance"
+  "bin/Radiance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Radiance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
